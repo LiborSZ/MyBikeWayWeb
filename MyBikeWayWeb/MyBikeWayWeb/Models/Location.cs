fdca7d2cacab4cebd3ed698335016e97ab8849ee
LiborSZ <reins20@seznam.cz>
2022-04-01 18:13:40 +0200

    Model class Location created with Id,Name,Latitude and longtitude properties

---
+++ MyBikeWayWeb/MyBikeWayWeb/Models/Location.cs
@@ -0,0 +1,23 @@
+﻿namespace MyBikeWayWeb.Models
+{
+    public class Location
+    {
+        /// <summary>
+        /// Database index
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// Location name
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// Latitude geographic coordinate
+        /// </summary>
+        public double Latitude { get; set; }
+        /// <summary>
+        /// Longtitude geographic coordinate
+        /// </summary>
+        public double Longtitude { get; set; }
+
+    }
+}
