891de7bbf2259391cd53832d37f5b62ba8053288
LiborSZ <reins20@seznam.cz>
2022-04-07 11:25:41 +0200

    Location attributes updated

--- MyBikeWayWeb/MyBikeWayWeb/Models/Location.cs
+++ MyBikeWayWeb/MyBikeWayWeb/Models/Location.cs
@@ -12,14 +12,18 @@ namespace MyBikeWayWeb.Models
         /// <summary>
         /// Location name
         /// </summary>
+        [Required]
+        [MaxLength(60,ErrorMessage = "Name is too long")]
         public string Name { get; set; }
         /// <summary>
         /// Latitude geographic coordinate
         /// </summary>
+        [Required]
         public double Latitude { get; set; }
         /// <summary>
         /// Longtitude geographic coordinate
         /// </summary>
+        [Required]
         public double Longtitude { get; set; }
 
     }
